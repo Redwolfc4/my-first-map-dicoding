d49bd20a8be734b2e5eaa1d1940b7350c1cbdc29
Salahudin Kholik Prasetyono <Salahudinkoliq10@gmail.com>
2025-05-03 22:56:20 +0700

    end

--- scripts/index.js
+++ scripts/index.js
@@ -19,6 +19,8 @@ baseTile.addTo(myMap);
 // Marker
 // const jakartaMarker = L.marker(jakartaCoor); //default
 const jakartaMarker = L.marker(jakartaCoor, {
+  title: "Jakarta",
+  alt: "Jakarta, capital city of Indonesia",
   icon: L.icon({
     iconUrl: "images/custom-marker-icons/leaf-green.png",
     iconSize: [38, 95],
@@ -28,6 +30,15 @@ const jakartaMarker = L.marker(jakartaCoor, {
     shadowAnchor: [4, 62],
     popupAnchor: [-2, -76],
   }),
+  draggable: true, //can drag it
+  // opacity: 0.5, //opacity
+  // zIndex: 100, //z index
+  // autoPan: true, //auto pan when zooming in
+  // autoPanPadding: [10, 10], //padding when auto pan
+  // autoPanPaddingTopLeft: [10, 10], //top left padding
+  // autoPanPaddingBottomRight: [10, 10], //bottom right padding
+  // autoPanSpeed: 100, //speed when auto pan
+  // rotation: 45
 }); //custom icon
 jakartaMarker.addTo(myMap);
 
@@ -48,3 +59,12 @@ myMap.on("click", (event) => {
   myNewPopup.setContent(`You clicked the map at ${event.latlng.toString()}`);
   myNewPopup.openOn(myMap);
 });
+
+jakartaMarker.addEventListener("move", (event) => {
+  console.log(event);
+  const position = event.target.getLatLng();
+  event.target.bindPopup(position.toString()).openPopup();
+});
+
+// Somewhere for changing marker location which will trigger event
+jakartaMarker.setLatLng([-6.1783, 106.6319]);
